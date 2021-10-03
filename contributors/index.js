a4037d907edbab040453c9449b32de55a6847a2a
Lakindu Kariyawasam <lakindu1997@gmail.com>
2021-10-03 10:40:06 +0530

    url updated

--- contributors/index.js
+++ contributors/index.js
@@ -2,7 +2,7 @@ var users = []
 var i = 0
 
 var getData = (function ($) {
-  var URL = 'https://api.github.com/repos/LakinduK/Codes-in-C/contributors'
+  var URL = 'https://api.github.com/repos/LakinduK/hacktoberfest2021'
 
   $.get(URL, function (data, status) {
     data.forEach(function (d) {
