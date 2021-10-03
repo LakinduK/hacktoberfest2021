be96d87d0a6c815660851c352b9ce784d5d04709
Lakindu Kariyawasam <lakindu1997@gmail.com>
2021-10-03 10:32:46 +0530

    Initial commit

---
+++ contributors/app.js
@@ -0,0 +1,68 @@
+// @path-json can be an object or an array, the first will be loaded directly, the object from the array will be random selected
+/* tsParticles.loadJSON(@dom-id, @path-json, @callback (optional)); */
+
+tsParticles
+  .loadJSON("tsparticles", "src/particles.json")
+  .then((container) => {
+    console.log("callback - tsparticles config loaded");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+//or
+
+/* tsParticles.load(@dom-id, @options); */
+
+tsParticles.load("tsparticles", {
+  /* options here */
+});
+
+//or
+
+/* tsParticles.loadFromArray(@dom-id, @options, @index (optional)); */
+
+tsParticles.loadFromArray("tsparticles", [
+  {
+    /* options here */
+  },
+  {
+    /* other options here */
+  },
+]);
+//random object
+
+tsParticles.loadFromArray(
+  "tsparticles",
+  [
+    {
+      /* options here */
+    },
+    {
+      /* other options here */
+    },
+  ],
+  1
+); //the second one
+// Important! If the index is not in range 0...<array.length, the index will be ignored.
+
+// after initialization this can be used.
+
+/* tsParticles.setOnClickHandler(@callback); */
+
+/* this will be fired from all particles loaded */
+
+tsParticles.setOnClickHandler((event, particles) => {
+  /* custom on click handler */
+});
+
+// now you can control the animations too, it's possible to pause and resume the animations
+// these methods don't change the config so you're safe with all your configurations
+// domItem(0) returns the first tsParticles instance loaded in the dom
+const particles = tsParticles.domItem(0);
+
+// play will start the animations, if the move is not enabled it won't enable it, it just updates the frame
+particles.play();
+
+// pause will stop the animations
+particles.pause();
---
+++ contributors/index.js
@@ -0,0 +1,30 @@
+var users = []
+var i = 0
+
+var getData = (function ($) {
+  var URL = 'https://api.github.com/repos/LakinduK/Codes-in-C/contributors'
+
+  $.get(URL, function (data, status) {
+    data.forEach(function (d) {
+      if (d.login == null) {
+        return true
+      }
+
+      if (users.indexOf(d.url) == -1) {
+	    var template =
+	    "<div class='col-md-3 col-xs-6 col-lg-3 zoom' id='author'><div class='container-fluid'>" +
+		    "<a class='thumbnail' target='_blank' href='" + d.html_url + "'>" +
+			    "<img src='" + d.avatar_url + "' alt='' class='img-responsive' style='border-radius: 20%; border-width: 0;'>" +
+		    '</a>' +
+		    "<div class='caption'>" +
+			    '<strong>' + d.login + '</strong>' +
+			    '<p> Contributions: ' + d.contributions + '</p>' +
+		    '</div></div></div>'
+
+        $('#contributors').append(template)
+        users[i] = d.url
+        i = i + 1
+      }
+    })
+  })
+})($)
